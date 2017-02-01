e273a2c93c43bd02213199018793764074377fac
Arvid Langsø <arvid.k.langsoe@live.dk>
2017-02-01 13:23:36 +0100

    Added comments to the files

--- TCPServerClient/src/Client.java
+++ TCPServerClient/src/Client.java
@@ -7,10 +7,11 @@ public class Client {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
+		//Get server ip from user.
 		System.out.println("Enter server ip:");
 		String server = scanner.nextLine();
 		
-		
+		//Get port number from user.
 		System.out.println("Enter port number:");
 		int port = scanner.nextInt();
 		
@@ -20,20 +21,27 @@ public class Client {
 			String sentence;
 			String modifiedSentence;
 			
+			//Creates a new socket that connects to the specified server.
 			System.out.println("Connecting to " + server + " on port " + port);
 			Socket clientSocket = new Socket(server, port);
 			
 			System.out.println("Connected to " + clientSocket.getRemoteSocketAddress() + " successfully!\n");
 			System.out.println("Write something to the server:");
+			
+			//Set up readers and output stream to the server.
 			BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			
+			//Read the input from the user. Holds program until a message is available.
 			sentence = inFromUser.readLine();
 			
+			//Write message to the server.
 			outToServer.writeBytes(sentence + '\n');
+			//Read the answer from the server. Holds program until a message is available.
 			modifiedSentence = inFromServer.readLine();
-
+			
+			//Print message from the server.
 			System.out.println("Server says: " + modifiedSentence);
 			clientSocket.close();
 			scanner.close();
